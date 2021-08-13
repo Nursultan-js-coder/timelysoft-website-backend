24905bc7f3d488cb70994b4cc817eb6e56a87cd8
Lukmanov <ozggldd@gmail.com>
2021-08-14 00:56:35 +0600

    added mongo atlas

--- config/database.js
+++ config/database.js
@@ -5,15 +5,10 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        uri: "mongodb+srv://nurs:2000nura@cluster0.vr8io.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
-        ssl: env("DATABASE_SSL"),
+        ssl: true,
       },
     },
   },
